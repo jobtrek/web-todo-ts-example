9f549012956cfb36c00bdb228e8adc510eb48f59
fuzoh <info@bastiennicoud.ch>
2025-01-14 13:23:21 +0100

    feat: create todolist html elements

---
+++ src/todoList.ts
@@ -0,0 +1,15 @@
+/**
+ * This function initializes the todo list application.
+ * @param todoInput
+ * @param dueDateInput
+ * @param addTodoButton
+ * @param todoListDisplay
+ */
+export const startTodoListApplication = (
+  todoInput: HTMLInputElement,
+  dueDateInput: HTMLInputElement,
+  addTodoButton: HTMLButtonElement,
+  todoListDisplay: HTMLUListElement
+) => {
+  console.log("Starting todo list application")
+}
