e2da473cbf59d0b97a76f22c304ea2b12ab47ec6
fuzoh <info@bastiennicoud.ch>
2025-02-21 06:30:17 +0100

    feat: disable add todo button if input is empty

--- src/todoList.ts
+++ src/todoList.ts
@@ -17,6 +17,9 @@ export const startTodoListApplication = (
   console.log('Starting todo list application')
   const todos = load()
 
+  /**
+   * Events for todo creation
+   */
   todoInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
       addTodoHandler(
@@ -30,5 +33,12 @@ export const startTodoListApplication = (
 
   addTodoButton.addEventListener('click', () => {
     addTodoHandler(todos, todoInput.value, dueDateInput.value, todoListDisplay)
+  })
+
+  /**
+   * Event to disable button when no text is present
+   */
+  todoInput.addEventListener('input', () => {
+    addTodoButton.disabled = todoInput.value.length < 1
   })
 }
